d5d00d8d9e717bec7072ca8e824ff1ee5b928539
Roberto Antunes <roh.antunes35@gmail.com>
2023-06-02 11:51:44 -0300

    Reorganizacao das pastas com extends

---
+++ .history/backend/static/js/script_20230602115105.js
@@ -0,0 +1,20 @@
+
+s$(document).ready(function() {
+    $('#btnBuscar').click(function(event) {
+        event.preventDefault();
+
+        var inputValue = $('#codigo').val();
+        console.log(inputValue);
+
+    $.ajax({
+        url: '/busca_produtos',
+        type: 'POST',
+        data: {codigoProduto: inputValue},
+        success: function(date) {
+            $('#descricao').val(date.updatedValue.descricao)
+            $('#codigoFornecedor').val(date.updatedValue.codigoFornecedor)
+            $('#unidade').val(date.updatedValue.unidade)
+        }
+    });
+    });
+});
